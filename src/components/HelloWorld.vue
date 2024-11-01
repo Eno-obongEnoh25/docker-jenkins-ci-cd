3697c927eba1a5eb908489e4e4e899113233fcbf
EnoobongEnoh25 <enoobongenoh25@gmail.com>
2024-11-01 18:28:07 +0100

    change index

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,9 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      enoobongenoh.com
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      enoobongetukenoh.com
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Docker</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Ninja</a>.
     </h3>
   </div>
 </template>
